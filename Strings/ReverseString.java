c578a0d76b5b897143c7479c733d1170daca57c2
mahekamesar <153438433+mahekamesar@users.noreply.github.com>
2024-05-14 20:09:19 +0530

    Update ReverseString.java

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,26 +1,42 @@
-//This code is related to reversing alphabets in a string
-//Eg: "Hello" will be reversed to "olleh"
+package Strings;
 
-public class StringPrograms {
+public class ReverseString {
+    public static void main(String[] args) {
+       /* We have 4 methods to reverse a string
+        1) By Using Char array
+        2) By using charAt() function
+        3) By using reverse() of String Builder class
+        4) By using reverse() of String Buffer class*/
+        String str = "Hello";
+        reverseByCharArray(str);
+        reverseByCharAt(str);
+        reverseByStringBuilder(str);
+        reverseByStringBuffer(str);
+    }
+    public static void reverseByCharArray(String str){
+        char[] charArr = str.toCharArray();
+        System.out.println("Reverse By Approach 1");
+        for (int i=str.length()-1;i>=0;i--){
+            System.out.print(charArr[i]+" ");
+        }
+    }
 
-	public static void main(String[] args) {
-		String str = "123";
+    public static void reverseByCharAt(String str){
+        System.out.println("Reverse By Approach 2");
+        for (int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
 
-		System.out.println(reverse(str));
-	}
+    public static void reverseByStringBuilder(String str){
+        System.out.println("Reverse By Approach 3");
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
+    }
 
-	public static String reverse(String in) {
-		if (in == null)
-			throw new IllegalArgumentException("Null is not valid input");
-
-		StringBuilder out = new StringBuilder();
-
-		char[] chars = in.toCharArray();
-
-		for (int i = chars.length - 1; i >= 0; i--)
-			out.append(chars[i]);
-
-		return out.toString();
-	}
-
+    public static void reverseByStringBuffer(String str){
+        System.out.println("Reverse By Approach 4");
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println(sb.reverse());
+    }
 }
